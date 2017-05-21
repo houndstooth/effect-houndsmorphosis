f544e326015aa76332940c35dbdd7ee975129c98
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2017-05-21 10:58:37 +0900

    center should not be a constant

--- utilities/adjustOrigin.js
+++ utilities/adjustOrigin.js
@@ -1,19 +1,19 @@
-import { CENTER } from '../../shared/constants'
 import state from '../../state'
 
 export default ({ initialOrigin, quarter, size }) => {
-	const { tileSize } = state.shared
+	const { tileSize, canvasSize } = state.shared
+	const canvasCenter = [ canvasSize / 2, canvasSize / 2 ]
 
 	const adjustedOrigin = [ null, null ]
 	if (quarter[ 0 ] === 1) {
-		adjustedOrigin[ 0 ] = CENTER[ 0 ] + initialOrigin[ 0 ]
+		adjustedOrigin[ 0 ] = canvasCenter[ 0 ] + initialOrigin[ 0 ]
 	} else {
-		adjustedOrigin[ 0 ] = CENTER[ 0 ] + (initialOrigin[ 0 ] + size) * quarter[ 0 ]
+		adjustedOrigin[ 0 ] = canvasCenter[ 0 ] + (initialOrigin[ 0 ] + size) * quarter[ 0 ]
 	}
 	if (quarter[ 1 ] === 1) {
-		adjustedOrigin[ 1 ] = CENTER[ 1 ] + initialOrigin[ 1 ]
+		adjustedOrigin[ 1 ] = canvasCenter[ 1 ] + initialOrigin[ 1 ]
 	} else {
-		adjustedOrigin[ 1 ] = CENTER[ 1 ] + (initialOrigin[ 1 ] + size) * quarter[ 1 ]
+		adjustedOrigin[ 1 ] = canvasCenter[ 1 ] + (initialOrigin[ 1 ] + size) * quarter[ 1 ]
 	}
 	adjustedOrigin[ 0 ] /= tileSize
 	adjustedOrigin[ 1 ] /= tileSize
