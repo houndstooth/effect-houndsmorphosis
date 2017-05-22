081e3f81289eccf9407fcfb1478199e194f1f3ea
Person McPivot <person.mcpivot@pivotal.io>
2017-05-22 18:02:36 +0900

    cant get colors straight from constants anymore

--- components/houndsmorphosisTile.js
+++ components/houndsmorphosisTile.js
@@ -1,11 +1,13 @@
 import { SQUARE_TYPE_TO_COLORS_MAPPING } from '../../shared/constants'
 import tile from '../../shared/components/tile'
 import adjustOrigin from '../utilities/adjustOrigin'
+import state from '../../state'
 
 export default ({ origin: initialOrigin, options }) => {
 	const { initialSize, growingSize, squareType, layerSquareSizeBehavior, quarter } = options
 	const size = layerSquareSizeBehavior === 'STEADY' ? initialSize : growingSize
-	const colors = SQUARE_TYPE_TO_COLORS_MAPPING[ squareType ].slice()
+	const colorKeys = SQUARE_TYPE_TO_COLORS_MAPPING[ squareType ].slice()
+	const colors = [ state.shared[ colorKeys[ 0 ] ], state.shared[ colorKeys[ 1 ] ] ]
 	const origin = adjustOrigin({ initialOrigin, quarter, size })
 	const scaleFromGridCenter = true
 
