01f3264f5b7b6b3af1b61026348631c451f83f29
Douglas Blumeyer <dblumeyer@pivotal.io>
2017-05-23 12:28:22 +0900

    extract square type to colors code
    Signed-off-by: Douglas Blumeyer <dblumeyer@pivotal.io>

--- components/houndsmorphosisTile.js
+++ components/houndsmorphosisTile.js
@@ -1,13 +1,11 @@
-import { SQUARE_TYPE_TO_COLORS_MAPPING } from '../../shared/constants'
 import tile from '../../shared/components/tile'
 import adjustOrigin from '../utilities/adjustOrigin'
-import state from '../../state'
+import convertSquareTypeToColors from '../../shared/utilities/convertSquareTypeToColors'
 
 export default ({ origin: initialOrigin, options }) => {
 	const { initialSize, growingSize, squareType, layerSquareSizeBehavior, quarter } = options
 	const size = layerSquareSizeBehavior === 'STEADY' ? initialSize : growingSize
-	const colorKeys = SQUARE_TYPE_TO_COLORS_MAPPING[ squareType ].slice()
-	const colors = [ state.shared[ colorKeys[ 0 ] ], state.shared[ colorKeys[ 1 ] ] ]
+	const colors = convertSquareTypeToColors({ squareType })
 	const origin = adjustOrigin({ initialOrigin, quarter, size })
 	const scaleFromGridCenter = true
 
