bc78cfb851f24598af95fb20de71fd1709b59c77
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2017-06-03 17:12:22 +0900

    consolidate origin and sized unit functions

--- houndsmorphosisOriginAndSizedUnit.js
+++ houndsmorphosisOriginAndSizedUnit.js
@@ -1,26 +1,18 @@
 import transpositionUtilities from '../shared/utilities/transpositionUtilities'
 import mathUtilities from '../shared/utilities/mathUtilities'
 
-const calculateHoundsmorphosisOrigin = ({ address }) => {
+export default ({ address }) => {
     const addressX = address[ 0 ]
     const addressY = address[ 1 ]
     const size = Math.ceil(addressY / 2)
+
     let origin = [ 
         mathUtilities.trapezoidalNumber({ start: size, height: addressX }),
         mathUtilities.quarterSquareNumber(addressY + 1) + addressX * size
     ]
     origin = transpositionUtilities.adjustOrigin({ origin })
-    return origin
-}
 
-const calculateHoundsmorphosisSizedUnit = ({ address }) => {
-    const addressX = address[ 0 ]
-    const addressY = address[ 1 ]
-    const size = Math.ceil(addressY / 2)
-    return addressY % 2 === 0 ? addressX + size + 1 : size
-}
+    const sizedUnit = addressY % 2 === 0 ? addressX + size + 1 : size
 
-export default {
-    calculateHoundsmorphosisOrigin,
-    calculateHoundsmorphosisSizedUnit
+    return { origin, sizedUnit }
 }
