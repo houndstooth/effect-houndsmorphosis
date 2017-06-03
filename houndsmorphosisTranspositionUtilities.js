afdc24e9f449b4335737c4728fd7ebdf02a019f3
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2017-06-03 12:57:03 +0900

    extract mathy stuff into the new shared math utilities

--- houndsmorphosisTranspositionUtilities.js
+++ houndsmorphosisTranspositionUtilities.js
@@ -1,20 +1,13 @@
 import transpositionUtilities from '../shared/utilities/transpositionUtilities'
 import mathUtilities from '../shared/utilities/mathUtilities'
 
-const quarterSquareNumber = n => Math.floor(Math.pow(n, 2) / 4)
-
-const trapezoidalNumber = ({ start, height }) => {
-    const { triangularNumber } = mathUtilities
-    return triangularNumber(start + height) - triangularNumber(start)
-} 
-
 const calculateHoundsmorphosisOrigin = ({ address }) => {
     const addressX = address[ 0 ]
     const addressY = address[ 1 ]
     const size = Math.ceil(addressY / 2)
     let origin = [ 
-        trapezoidalNumber({ start: size, height: addressX }),
-        quarterSquareNumber(addressY + 1) + addressX * size
+        mathUtilities.trapezoidalNumber({ start: size, height: addressX }),
+        mathUtilities.quarterSquareNumber(addressY + 1) + addressX * size
     ]
     origin = transpositionUtilities.adjustOrigin({ origin })
     return origin
