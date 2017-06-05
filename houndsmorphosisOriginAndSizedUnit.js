b49aac069229889589f02e884c05877a1e6016e4
Douglas Blumeyer <dblumeyer@pivotal.io>
2017-06-05 14:07:27 +0900

    omg what a relief, why didnt i think of that no axes idea before
    Signed-off-by: Douglas Blumeyer <dblumeyer@pivotal.io>

--- houndsmorphosisOriginAndSizedUnit.js
+++ houndsmorphosisOriginAndSizedUnit.js
@@ -3,45 +3,28 @@ import mathUtilities from '../shared/utilities/mathUtilities'
 import state from '../shared/state/state'
 
 export default ({ address }) => {
-	const addressX = address[ 0 ]
-	const addressY = address[ 1 ]
+	if (address[ 0 ] === 0 || address[ 1 ] === 0) return { origin: null, sizedUnit: null }
 
-	// what if we solved the problem by eliminating every tile located on either axis ?
-	// that is, none of this preferring [ 0, 0 ] into the bottom right quadrant
-	// if (addressX === 0 || addressY === 0) return { origin: null, sizedUnit: null }
+	const addressX = Math.abs(address[ 0 ])
+	const addressY = Math.abs(address[ 1 ])
 
-	// base size is used in everything
-	const size = Math.abs(Math.ceil(addressY / 2))
+	const baseSize = Math.floor(addressY / 2)
 
-	// x
-	const thingOne = addressY < 0 ? Math.abs(addressX) : 0
-	const thingTwo = addressX < 0 ? Math.abs(addressX) : 0
-	const thing = addressY % 2 === 0 ? thingOne : thingTwo
-	let x = mathUtilities.trapezoidalNumber({
-			start: size,
-			height: Math.abs(addressX)
-		})
-		-
-		thing
+	let x = mathUtilities.trapezoidalNumber({ start: baseSize, height: addressX - 1 })
+	let y = mathUtilities.quarterSquareNumber(addressY) + (addressX - 1) * baseSize
 
-	// y
-	const sizeScalarX = addressX < 0 ? Math.abs(addressX + 1) : addressX
-	const sizeScalarY = addressY < 0 ? addressY : addressY + 1
-	let y = mathUtilities.quarterSquareNumber(sizeScalarY)
-		+
-		sizeScalarX * size
+	let size = addressY % 2 !== 0 ? addressX + baseSize : baseSize
 
-	// put in correct quadrant
-	if (addressX < 0) x *= -1
-	if (addressY < 0) y *= -1
+	if (address[ 0 ] < 0) {
+		x *= -1
+		x -= size
+	}
+	if (address[ 1 ] < 0) {
+		y *= -1
+		y -= size
+	}
 
-	// size
-	const scalarX = addressX < 0 ? -0.5 : 0.5
-	const scalarY = addressY < 0 ? -0.5 : 0.5
-	const growingSize = Math.abs(addressX) + size + scalarX + scalarY
-	let sizedUnit = addressY % 2 === 0 ? growingSize : size
-	sizedUnit *= state.unit
-
+	const sizedUnit = size * state.unit
 	const origin = transpositionUtilities.adjustOrigin({ origin: [ x, y ] })
 	return { origin, sizedUnit }
 }
