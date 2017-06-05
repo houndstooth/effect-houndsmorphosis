5feb3502319d879c4e1583f5f3b3d9cd08ea7d34
Douglas Blumeyer <dblumeyer@pivotal.io>
2017-06-05 16:11:48 +0900

    exports to top
    Signed-off-by: Douglas Blumeyer <dblumeyer@pivotal.io>

--- calculateHoundsmorphosisSupertile.js
+++ calculateHoundsmorphosisSupertile.js
@@ -2,27 +2,27 @@
 // a pattern exists here.
 
 export default ({ address }) => {
-    let supertile
-    if (address[0] >= 0 && address[1] >= 0) {
-        supertile = [
-            [ [ 0, 0 ], [ 0, 1 ] ], 
-            [ [ 1, 1 ], [ 1, 0 ] ]
-        ]
-    } else if (address[0] >= 0 && address[1] < 0) {
-        supertile = [
-            [ [ 1, 1 ], [ 1, 0 ] ], 
-            [ [ 0, 0 ], [ 0, 1 ] ]
-        ]
-    } else if (address[0] < 0 && address[1] >= 0) {
-        supertile = [ 
-            [ [ 1, 0 ], [ 1, 1 ] ],
-            [ [ 0, 1 ], [ 0, 0 ] ]
-        ]
-    } else if (address[0] < 0 && address[1] < 0) {
-        supertile = [ 
-            [ [ 0, 1 ], [ 0, 0 ] ],
-            [ [ 1, 0 ], [ 1, 1 ] ]
-        ]
-    }
-    return supertile
+	let supertile
+	if (address[ 0 ] >= 0 && address[ 1 ] >= 0) {
+		supertile = [
+			[ [ 0, 0 ], [ 0, 1 ] ],
+			[ [ 1, 1 ], [ 1, 0 ] ]
+		]
+	} else if (address[ 0 ] >= 0 && address[ 1 ] < 0) {
+		supertile = [
+			[ [ 1, 1 ], [ 1, 0 ] ],
+			[ [ 0, 0 ], [ 0, 1 ] ]
+		]
+	} else if (address[ 0 ] < 0 && address[ 1 ] >= 0) {
+		supertile = [
+			[ [ 1, 0 ], [ 1, 1 ] ],
+			[ [ 0, 1 ], [ 0, 0 ] ]
+		]
+	} else if (address[ 0 ] < 0 && address[ 1 ] < 0) {
+		supertile = [
+			[ [ 0, 1 ], [ 0, 0 ] ],
+			[ [ 1, 0 ], [ 1, 1 ] ]
+		]
+	}
+	return supertile
 }
--- houndsmorphosisPreset.js
+++ houndsmorphosisPreset.js
@@ -2,16 +2,16 @@ const CANVAS_SIZE = 1000
 
 export default {
 	state: {
-        gridSize: 71,
-        canvasSize: CANVAS_SIZE,
+		gridSize: 71,
+		canvasSize: CANVAS_SIZE,
 		houndsmorphosisMode: true,
-        colorConfig: {
-            assignment: {
-                mode: 'SUPERTILE'
-            }
-        },
-        offsetOrigin: [ CANVAS_SIZE / 2, CANVAS_SIZE / 2 ],
-        negativeGridToo: true,
-        scaleFromGridCenter: false
+		colorConfig: {
+			assignment: {
+				mode: 'SUPERTILE'
+			}
+		},
+		offsetOrigin: [ CANVAS_SIZE / 2, CANVAS_SIZE / 2 ],
+		negativeGridToo: true,
+		scaleFromGridCenter: false
 	}
 }
