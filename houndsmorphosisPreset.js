79f726722749879eea0993ddeedfb0c18840a8bc
Douglas Blumeyer <dblumeyer@pivotal.io>
2017-06-06 14:38:37 +0900

    do not name things 'function'
    Signed-off-by: Douglas Blumeyer <dblumeyer@pivotal.io>

--- houndsmorphosisPreset.js
+++ houndsmorphosisPreset.js
@@ -14,7 +14,7 @@ export default {
 					[ [ 0, 0 ], [ 0, 1 ] ],
 					[ [ 1, 1 ], [ 1, 0 ] ]
 				],
-				addressOffsetFunction: getHoundsmorphosisAddressOffset
+				offsetAddress: getHoundsmorphosisAddressOffset
 			}
 		},
 		offsetOrigin: [ CANVAS_SIZE / 2, CANVAS_SIZE / 2 ],
