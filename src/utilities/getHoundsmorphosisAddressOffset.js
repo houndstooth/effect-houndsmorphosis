170593712acb2ce6e2d31a8f2cbc4cf3844424e6
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2017-07-24 07:48:44 +0900

    address is gridAddress, to enforce clarity

--- src/utilities/getHoundsmorphosisAddressOffset.js
+++ src/utilities/getHoundsmorphosisAddressOffset.js
@@ -1,13 +1,13 @@
-export default ({ address }) => {
+export default ({ gridAddress }) => {
 	let xOffset = 0
 	let yOffset = 0
 
-	if (address[ 0 ] < 0) {
+	if (gridAddress[ 0 ] < 0) {
 		yOffset += 1
 		xOffset += 1
 	}
 
-	if (address[ 1 ] < 0) {
+	if (gridAddress[ 1 ] < 0) {
 		xOffset += 1
 	}
 
--- src/utilities/getHoundsmorphosisTileOriginAndSize.js
+++ src/utilities/getHoundsmorphosisTileOriginAndSize.js
@@ -1,10 +1,10 @@
 import mathUtilities from '../../../../src/utilities/mathUtilities'
 
-export default ({ address }) => {
-	if (address[ 0 ] === 0 || address[ 1 ] === 0) return { tileOrigin: null, tileSize: null }
+export default ({ gridAddress }) => {
+	if (gridAddress[ 0 ] === 0 || gridAddress[ 1 ] === 0) return { tileOrigin: null, tileSize: null }
 
-	const addressX = Math.abs(address[ 0 ])
-	const addressY = Math.abs(address[ 1 ])
+	const addressX = Math.abs(gridAddress[ 0 ])
+	const addressY = Math.abs(gridAddress[ 1 ])
 
 	const baseSize = Math.floor(addressY / 2)
 
@@ -13,11 +13,11 @@ export default ({ address }) => {
 
 	let tileSize = addressY % 2 !== 0 ? addressX + baseSize : baseSize
 
-	if (address[ 0 ] < 0) {
+	if (gridAddress[ 0 ] < 0) {
 		x *= -1
 		x -= tileSize
 	}
-	if (address[ 1 ] < 0) {
+	if (gridAddress[ 1 ] < 0) {
 		y *= -1
 		y -= tileSize
 	}
