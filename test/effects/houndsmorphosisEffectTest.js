8adae9ecd13c0dd46725eb69b4f0caae65f82947
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2017-09-09 18:46:12 +0900

    interface is now controls to avoid collision with reserved keyword

--- test/effects/houndsmorphosisEffectTest.js
+++ test/effects/houndsmorphosisEffectTest.js
@@ -1,4 +1,4 @@
-import executeSelectedHoundstoothEffects from '../../../../src/interface/executeSelectedHoundstoothEffects'
+import executeSelectedHoundstoothEffects from '../../../../src/controls/executeSelectedHoundstoothEffects'
 import standardTileIsColors from '../../../../test/integration/helpers/standardTileIsColors'
 import { BLACK, TRANSPARENT } from '../../../../src/constants'
 import houndsmorphosisEffect from '../../effects/houndsmorphosisEffect'
