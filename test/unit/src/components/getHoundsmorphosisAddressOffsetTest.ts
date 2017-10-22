ddb6d54a8cc62649f41e4517cdcff8492fd38dc2
Douglas <douglas.blumeyer@gmail.com>
2017-10-23 03:41:06 +0800

    base settings path nominal typing on a singular (non-array) type

--- test/unit/src/components/getHoundsmorphosisAddressOffsetTest.ts
+++ test/unit/src/components/getHoundsmorphosisAddressOffsetTest.ts
@@ -5,30 +5,30 @@ import { getHoundsmorphosisAddressOffset } from '../../../../src/components/getH
 describe('houndsmorphosis address offset deals with set assignment because the axes were eliminated to simplify sizing and positioning but that screws with assignment', () => {
 	describe('when both x and y are negative', () => {
 		it('provides an offset to cause the address to assign its tile the correct colors, etc', () => {
-			expect(getHoundsmorphosisAddressOffset({ gridAddress: to.Address([ -1, -1 ]) })).toEqual([ 2, 1 ])
+			expect(getHoundsmorphosisAddressOffset({ gridAddress: to.Address([ -1, -1 ]) })).toEqual(to.Address([ 2, 1 ]))
 		})
 	})
 
 	describe('when x is negative and y is 0 or positive', () => {
 		it('provides an offset to cause the address to assign its tile the correct colors, etc', () => {
-			expect(getHoundsmorphosisAddressOffset({ gridAddress: to.Address([ -1, 0 ]) })).toEqual([ 1, 1 ])
-			expect(getHoundsmorphosisAddressOffset({ gridAddress: to.Address([ -1, 1 ]) })).toEqual([ 1, 1 ])
+			expect(getHoundsmorphosisAddressOffset({ gridAddress: to.Address([ -1, 0 ]) })).toEqual(to.Address([ 1, 1 ]))
+			expect(getHoundsmorphosisAddressOffset({ gridAddress: to.Address([ -1, 1 ]) })).toEqual(to.Address([ 1, 1 ]))
 		})
 	})
 
 	describe('when x is 0 or positive and y is negative', () => {
 		it('provides an offset to cause the address to assign its tile the correct colors, etc', () => {
-			expect(getHoundsmorphosisAddressOffset({ gridAddress: to.Address([ 0, -1 ]) })).toEqual([ 1, 0 ])
-			expect(getHoundsmorphosisAddressOffset({ gridAddress: to.Address([ 1, -1 ]) })).toEqual([ 1, 0 ])
+			expect(getHoundsmorphosisAddressOffset({ gridAddress: to.Address([ 0, -1 ]) })).toEqual(to.Address([ 1, 0 ]))
+			expect(getHoundsmorphosisAddressOffset({ gridAddress: to.Address([ 1, -1 ]) })).toEqual(to.Address([ 1, 0 ]))
 		})
 	})
 
 	describe('when both x and y are 0 or positive', () => {
 		it('does not offset', () => {
-			expect(getHoundsmorphosisAddressOffset({ gridAddress: to.Address([ 0, 0 ]) })).toEqual([ 0, 0 ])
-			expect(getHoundsmorphosisAddressOffset({ gridAddress: to.Address([ 1, 0 ]) })).toEqual([ 0, 0 ])
-			expect(getHoundsmorphosisAddressOffset({ gridAddress: to.Address([ 0, 1 ]) })).toEqual([ 0, 0 ])
-			expect(getHoundsmorphosisAddressOffset({ gridAddress: to.Address([ 1, 1 ]) })).toEqual([ 0, 0 ])
+			expect(getHoundsmorphosisAddressOffset({ gridAddress: to.Address([ 0, 0 ]) })).toEqual(to.Address([ 0, 0 ]))
+			expect(getHoundsmorphosisAddressOffset({ gridAddress: to.Address([ 1, 0 ]) })).toEqual(to.Address([ 0, 0 ]))
+			expect(getHoundsmorphosisAddressOffset({ gridAddress: to.Address([ 0, 1 ]) })).toEqual(to.Address([ 0, 0 ]))
+			expect(getHoundsmorphosisAddressOffset({ gridAddress: to.Address([ 1, 1 ]) })).toEqual(to.Address([ 0, 0 ]))
 		})
 	})
 })
